8736404da064013594c3c89ba1e04ac9a1d67053
SEMBARANG <ahmadsulthoni87@gmail.com>
2022-03-12 16:29:16 +0700

    commit steampowered pertama

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import panda as pd
 
 url = 'https://store.steampowered.com/search/?term=gta'
 
-params = {
-    'term' : 'gta'
-}
+def get_data(url):
+    r = requests.get(url)
+    return r.text
+
+#prosessing data
+def parse(data):
+    result = []
+    soup = BeautifulSoup(data, 'html.parser')
 
 
