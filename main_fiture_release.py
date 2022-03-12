2a4117febe9e407184ad4ecab1f3ea858799c898
SEMBARANG <ahmadsulthoni87@gmail.com>
2022-03-12 20:42:31 +0700

    Menambah Data Released

---
+++ main_fiture_release.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import pandas as pd
+
+url = 'https://store.steampowered.com/search/?term=gta'
+
+def get_data(url):
+    r = requests.get(url)
+    return r.text
+
+#prosessing data
+def parse(data):
+    result = []
+    soup = BeautifulSoup(data, 'html.parser')
+    contents = soup.find('div',attrs={'id' : 'search_resultsRows'})
+    games = contents.find_all('a')
+
+    for game in games:
+        link = game['href']
+
+        #parsing data
+        title = game.find('span', {'class':'title'}).text.strip().split('$')[0]
+        price = game.find('div',{'class': 'search_price'}).text.strip().split('$')[0]
+        released = game.find('div',{'class' : 'search_released'}).text.strip().split('$')[0]
+        if released == '':
+            released = 'none'
+
+        #sorting_data
+        data_dict = {
+            'title' : title,
+            'price' : price,
+            'link' : link,
+            'released' : released,
+        }
+
+        #append untuk menampung data
+        result.append(data_dict)
+    return result
+
+#proses cleaned data from parser
+def output (datas : list):
+    for i in datas:
+        print(i)
+
+
+if __name__ == '__main__':
+    data = get_data(url)
+
+    final_data = parse(data)
+
+    output(final_data)
+
+
